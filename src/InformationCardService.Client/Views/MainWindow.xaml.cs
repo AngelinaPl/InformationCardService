166a4e3493c62a4e4867de8361ffa2e748608678
AngelinaPl <angelinka.1999.17@mail.ru>
2020-02-21 13:35:25 +0300

    Initial commit

---
+++ src/InformationCardService.Client/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using InformationCardService.Client.ViewModels;
+
+namespace InformationCardService.Client.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
